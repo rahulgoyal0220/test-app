8698826d93530dfa499ca7015cd1d769a17ed805
rahul goyal <rahulgoyal@rahuls-MacBook-Pro.local>
2020-09-13 23:14:22 -0400

    script

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/api/user', user);
 
+app.get("/", (req, res) => {
+    res.send({message : "welcome"});
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
